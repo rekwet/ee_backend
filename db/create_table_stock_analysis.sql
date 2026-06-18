37802d2d4d0e3c4160434437473e520bdd6a7f60
rekwet <31699450+rekwet@users.noreply.github.com>
2026-06-18 05:40:20 +0000

    add user_id

--- db/create_table_stock_analysis.sql
+++ db/create_table_stock_analysis.sql
@@ -1,13 +1,14 @@
-CREATE TABLE public.stock_analyses (
+CREATE TABLE IF NOT EXISTS public.stock_analyses (
     id SERIAL PRIMARY KEY,
+    user_id VARCHAR(200) NOT NULL DEFAULT ('DEMO'),
     ticker VARCHAR(12) NOT NULL,
     exchange VARCHAR(20),
     market_cap NUMERIC(20, 2),
     current_price NUMERIC(12, 2) NOT NULL,
     intrinsic_value NUMERIC(12, 2),
     peg_ratio NUMERIC(8, 4),
-    total_score VARCHAR(10) NOT NULL, -- Stores fractional scores like '5/12'
-    conclusion VARCHAR(20) NOT NULL,  -- Stores outputs like 'BUY', 'HOLD', 'SELL'
+    total_score VARCHAR(10) NOT NULL,
+    conclusion VARCHAR(20) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
